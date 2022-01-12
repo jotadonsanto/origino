7782949d735720ff20d096444c1cb3b250c78db1
Julian Donsanto <julian.donsanto@stensul.com>
2022-01-12 18:01:10 -0300

    carga_manual vista

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Route } from 'react-router-dom'
+import { BrowserRouter as Route, Switch } from 'react-router-dom'
 import { StyledContainer, StyledDrawer, StyledBox } from './Main.styles';
 import { ThemeProvider } from '@mui/material/styles';
 import { StyledEngineProvider } from '@mui/material/styles';
@@ -11,6 +11,7 @@ import Sidebar from '../components/Sidebar/Sidebar';
 import Activos from '../pages/activos/Activos';
 import ActivosCarga from '../pages/activos/ActivosCarga';
 import ArchivoCarga from '../pages/archivos/ArchivoCarga';
+import ArchivoCargaManual from '../pages/archivos/ArchivoCargaManual';
 import ArchivoVerificacion from '../pages/archivos/ArchivoVerificacion';
 
 const pages = [
@@ -21,11 +22,6 @@ const pages = [
   },
   {
     exact: true,
-    path: '/archivo/verificacion',
-    component: ArchivoVerificacion,
-  },
-  {
-    exact: true,
     path: '/activos/carga',
     component: ActivosCarga,
   },
@@ -33,6 +29,16 @@ const pages = [
     exact: true,
     path: '/archivo/carga',
     component: ArchivoCarga,
+  },
+  {
+    exact: true,
+    path: '/archivo/carga_manual',
+    component: ArchivoCargaManual,
+  },
+  {
+    exact: true,
+    path: '/archivo/verificacion',
+    component: ArchivoVerificacion,
   },
 ]
 
@@ -51,11 +57,13 @@ function Main() {
             component="main">
             <Header />
             <StyledContainer>
+              <Switch>
               {pages.map((page, index) => (
                 <Route key={index} path={page.path} exact>
                   <page.component/>
                 </Route>
               ))}
+              </Switch>
             </StyledContainer>
           </StyledBox>
         </Box>
---
+++ src/pages/archivos/ArchivoCargaManual.jsx
@@ -0,0 +1,128 @@
+import React from 'react';
+import clsx from 'clsx';
+import { Grid, Stepper, Step, StepButton, Divider, Button, Checkbox, FormControlLabel, Card, Typography } from '@mui/material';
+import { DataGrid } from '@mui/x-data-grid';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faExclamationCircle } from '@fortawesome/pro-light-svg-icons';
+import TableCell from '../../components/archivos/verificacion/TableCell';
+// This is only for test purposes, delete when fetching real data
+import { randomCreatedDate, randomTraderName } from '@mui/x-data-grid-generator';
+
+function ArchivoCargaManual() {
+  // For Stepper
+  const steps = ['Cargar Archivo', 'Verificación', 'Resumen y confirmación'];
+  const [activeStep, setActiveStep] = React.useState(0);
+  const [completed] = React.useState({});
+  const handleStep = (step) => () => {
+    setActiveStep(step);
+  };
+
+  // For error cell - apply real validation here.
+  const hasErrors = (params) => (params.value > 130 ? true : false);
+  // Check that the error has been fixed to return true. Now its fake.
+  const isFixed = (params) => (params.value === 130 ? true : false);
+
+  // For error popover
+  const [errorCell, setErrorCell] = React.useState(null);
+  const errorOpen = Boolean(errorCell);
+  const showError = (event) => { setErrorCell(event.currentTarget) };
+  const closeError = () => { setErrorCell(null);};
+  const errorId = errorOpen ? 'error-popover' : undefined;
+
+  // For ApplyAll popover
+  const [applyAllCell, setApplyAllCell] = React.useState(null);
+  const applyAllOpen = Boolean(applyAllCell);
+  const showApplyAll = (event) => { setApplyAllCell(event.currentTarget) };
+  const closeApplyAll = () => { setApplyAllCell(null);};
+  const applyAllId = applyAllOpen ? 'apply-all-popover' : undefined;
+
+  const rows = [
+    {
+      id: 1,
+      fecha: '',
+      id_caravana: 1,
+      especie: '',
+      marca_fuego: '',
+      raza: '',
+      categoria: '',
+      denticion: 0,
+      sistema_productivo: '',
+      lote: '',
+      peso: '',
+    },
+    {
+      id: 0,
+      fecha: '',
+      id_caravana: 2,
+      especie: '',
+      marca_fuego: '',
+      raza: '',
+      categoria: '',
+      denticion: 0,
+      sistema_productivo: '',
+      lote: '',
+      peso: '',
+    },
+  ];
+  const columns = [
+    { field: 'fecha', headerName: 'Fecha', type: 'date', flex: 1, editable: true, align: 'center', headerAlign: 'center' },
+    { field: 'id_caravana', headerName: 'ID de Caravana', flex: 1,  type: 'number', editable: true, align: 'center', headerAlign: 'center' },
+    { field: 'especie', headerName: 'Especie', flex: 1, editable: true, align: 'center', headerAlign: 'center' },
+    { field: 'marca_fuego', headerName: 'Marca de Fuego', flex: 1, editable: true, align: 'center', headerAlign: 'center' },
+    { field: 'raza', headerName: 'Raza', flex: 1, editable: true, align: 'center', headerAlign: 'center' },
+    { field: 'categoria', headerName: 'Categoria', flex: 1, editable: true, align: 'center', headerAlign: 'center' },
+    { field: 'denticion', headerName: 'Dentición', type: 'number', flex: 1, editable: true, align: 'center', headerAlign: 'center' },
+    { field: 'sistema_productivo', headerName: 'Sistema productivo', type: 'singleSelect', valueOptions: ['Pastura', 'Otros', 'Otros 2'], flex: 1, editable: true, align: 'center', headerAlign: 'center' },
+    { field: 'lote', headerName: 'Lote', type: 'number', flex: 1, editable: true, align: 'center', headerAlign: 'center' },
+    { field: 'peso', headerName: 'Peso (KG)', flex: 1, editable: true, align: 'center', headerAlign: 'center',
+      cellClassName: (params) => clsx({'pl-0 pr-0 overflow-visible': hasErrors(params) || isFixed(params)}),
+      renderCell:(params) => {
+        return <TableCell
+          hasErrors={hasErrors} isFixed={isFixed} params={params}
+          showError={showError} errorId={errorId}
+          errorOpen={errorOpen} errorCell={errorCell}
+          closeError={closeError} applyAllId={applyAllId}
+          applyAllOpen={applyAllOpen} applyAllCell={applyAllCell}
+          closeApplyAll={closeApplyAll} />
+      }},
+  ];
+
+  return (
+    //Starts the route view with <Grid container> and add <Grid item 12> for rows.
+    <Grid container spacing={2}>
+      <Grid item xs={12} className="mt-3">
+        <Stepper nonLinear activeStep={activeStep}>
+          {steps.map((label, index) => (
+            <Step key={label} completed={completed[index]}>
+              <StepButton color="inherit" onClick={handleStep(index)}>
+                {label}
+              </StepButton>
+            </Step>
+          ))}
+        </Stepper>
+        <Divider variant="middle" className="mt-4"/>
+      </Grid>
+      <Grid item xs={12}>
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          hideFooterSelectedRowCount
+          pFiltroSize={5}
+          rowsPerPFiltroOptions={[5]}
+          disableColumnFilter
+          disableColumnMenu
+          disableSelectionOnClick
+          autoHeight
+          onCellEditCommit={(params, event, details) => {
+            console.log(params, event, details);
+            showApplyAll(event);
+          }} />
+      </Grid>
+      <Grid item xs={12} className="d-flex justify-center">
+        <Button variant="contained" disabled>Continuar</Button>
+      </Grid>
+    </Grid>
+  );
+}
+
+export default ArchivoCargaManual;
