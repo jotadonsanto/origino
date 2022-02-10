8de537523d7d49a668d8a5da6952d249f2d308ac
Julian Donsanto <julian.donsanto@stensul.com>
2022-02-10 19:15:53 -0300

    Vista de correccion de columnas

--- src/pages/archivos/ArchivoCargaManual.jsx
+++ src/pages/archivos/ArchivoCargaManual.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import clsx from 'clsx';
-import { Grid, Stepper, Step, StepButton, Divider, Button } from '@mui/material';
+import { Grid, Menu, MenuItem, Stepper, Step, StepButton, Divider, Button, IconButton } from '@mui/material';
 import { DataGrid } from '@mui/x-data-grid';
-import TableCell from '../../components/archivos/verificacion/TableCell';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronDown } from '@fortawesome/pro-light-svg-icons';
+import ErrorCell from './../../components/archivos/verificacion/ErrorCell';
 
 function ArchivoCargaManual() {
   // For Stepper
@@ -13,74 +14,98 @@ function ArchivoCargaManual() {
     setActiveStep(step);
   };
 
-  // For error cell - apply real validation here.
-  const hasErrors = (params) => (params.value > 130 ? true : false);
-  // Check that the error has been fixed to return true. Now its fake.
-  const isFixed = (params) => (params.value === 130 ? true : false);
-
-  // For error popover
-  const [errorCell, setErrorCell] = React.useState(null);
-  const errorOpen = Boolean(errorCell);
-  const showError = (event) => { setErrorCell(event.currentTarget) };
-  const closeError = () => { setErrorCell(null);};
-  const errorId = errorOpen ? 'error-popover' : undefined;
-
-  // For ApplyAll popover
-  const [applyAllCell, setApplyAllCell] = React.useState(null);
-  const applyAllOpen = Boolean(applyAllCell);
-  const showApplyAll = (event) => { setApplyAllCell(event.currentTarget) };
-  const closeApplyAll = () => { setApplyAllCell(null);};
-  const applyAllId = applyAllOpen ? 'apply-all-popover' : undefined;
+  // For table dropdown
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   const rows = [
     {
       id: 1,
-      fecha: '',
-      id_caravana: 1,
-      especie: '',
-      marca_fuego: '',
-      raza: '',
-      categoria: '',
-      denticion: 0,
-      sistema_productivo: '',
-      lote: '',
-      peso: '',
+      fecha: 'Esto deberia ser una fecha',
+      id_caravana: 'Testingggg',
+      denticion: 'Test',
     },
     {
       id: 0,
-      fecha: '',
-      id_caravana: 2,
-      especie: '',
-      marca_fuego: '',
-      raza: '',
-      categoria: '',
-      denticion: 0,
-      sistema_productivo: '',
-      lote: '',
-      peso: '',
+      fecha: 'Esto deberia ser una fecha',
+      id_caravana: 'Testingggg',
+      denticion: 'Test',
     },
   ];
   const columns = [
-    { field: 'fecha', headerName: 'Fecha', type: 'date', flex: 1, editable: true, align: 'center', headerAlign: 'center' },
-    { field: 'id_caravana', headerName: 'ID de Caravana', flex: 1,  type: 'number', editable: true, align: 'center', headerAlign: 'center' },
-    { field: 'especie', headerName: 'Especie', flex: 1, editable: true, align: 'center', headerAlign: 'center' },
-    { field: 'marca_fuego', headerName: 'Marca de Fuego', flex: 1, editable: true, align: 'center', headerAlign: 'center' },
-    { field: 'raza', headerName: 'Raza', flex: 1, editable: true, align: 'center', headerAlign: 'center' },
-    { field: 'categoria', headerName: 'Categoria', flex: 1, editable: true, align: 'center', headerAlign: 'center' },
-    { field: 'denticion', headerName: 'Dentición', type: 'number', flex: 1, editable: true, align: 'center', headerAlign: 'center' },
-    { field: 'sistema_productivo', headerName: 'Sistema productivo', type: 'singleSelect', valueOptions: ['Pastura', 'Otros', 'Otros 2'], flex: 1, editable: true, align: 'center', headerAlign: 'center' },
-    { field: 'lote', headerName: 'Lote', type: 'number', flex: 1, editable: true, align: 'center', headerAlign: 'center' },
-    { field: 'peso', headerName: 'Peso (KG)', flex: 1, editable: true, align: 'center', headerAlign: 'center',
-      cellClassName: (params) => clsx({'pl-0 pr-0 overflow-visible': hasErrors(params) || isFixed(params)}),
+    { field: 'fecha', headerName: 'Fecha', type: 'date', flex: 1, editable: true, align: 'center', headerAlign: 'center', sortable: false,
+      renderHeader: (params) => {
+        return (
+          <React.Fragment>
+            { params.colDef.headerName }
+            <IconButton
+              size="small"
+              className="ml-4"
+              id="basic-button"
+              aria-controls="basic-menu"
+              aria-label="Expand"
+              aria-expanded={open ? 'true' : undefined}
+              onClick={handleClick}>
+              <FontAwesomeIcon icon={faChevronDown} />
+            </IconButton>
+          </React.Fragment>
+        )
+      },
       renderCell:(params) => {
-        return <TableCell
-          hasErrors={hasErrors} isFixed={isFixed} params={params}
-          showError={showError} errorId={errorId}
-          errorOpen={errorOpen} errorCell={errorCell}
-          closeError={closeError} applyAllId={applyAllId}
-          applyAllOpen={applyAllOpen} applyAllCell={applyAllCell}
-          closeApplyAll={closeApplyAll} />
-      }},
+        return <ErrorCell params={params} />
+      },
+    },
+    { field: 'id_caravana', headerName: 'ID de Caravana', flex: 1,  type: 'number', editable: true, align: 'center', headerAlign: 'center', sortable: false,
+      renderHeader: (params) => {
+        return (
+          <React.Fragment>
+            { params.colDef.headerName }
+            <IconButton
+              size="small"
+              className="ml-4"
+              id="basic-button"
+              aria-controls="basic-menu"
+              aria-label="Expand"
+              aria-expanded={open ? 'true' : undefined}
+              onClick={handleClick}>
+              <FontAwesomeIcon icon={faChevronDown} />
+            </IconButton>
+          </React.Fragment>
+        )
+      },
+      renderCell:(params) => {
+        return <ErrorCell params={params} />
+      },
+    },
+    { field: 'denticion', headerName: 'Dentición', type: 'number', flex: 1, editable: true, align: 'center', headerAlign: 'center', sortable: false,
+    renderHeader: (params) => {
+      console.log(params);
+      return (
+        <React.Fragment>
+          { params.colDef.headerName }
+          <IconButton
+            size="small"
+            className="ml-4"
+            id="basic-button"
+            aria-controls="basic-menu"
+            aria-label="Expand"
+            aria-expanded={open ? 'true' : undefined}
+            onClick={handleClick}>
+            <FontAwesomeIcon icon={faChevronDown} />
+          </IconButton>
+        </React.Fragment>
+      )
+    },
+    renderCell:(params) => {
+      return <ErrorCell params={params} />
+    },
+  },
   ];
 
   return (
@@ -108,11 +133,20 @@ function ArchivoCargaManual() {
           disableColumnFilter
           disableColumnMenu
           disableSelectionOnClick
-          autoHeight
-          onCellEditCommit={(params, event, details) => {
-            console.log(params, event, details);
-            showApplyAll(event);
-          }} />
+          autoHeight/>
+          <Menu
+            id="data-grid-menu"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            MenuListProps={{
+              'aria-labelledby': 'basic-button',
+            }}
+          >
+            <MenuItem onClick={handleClose}>Select</MenuItem>
+            <MenuItem onClick={handleClose}>The Correct</MenuItem>
+            <MenuItem onClick={handleClose}>Category</MenuItem>
+          </Menu>
       </Grid>
       <Grid item xs={12} className="d-flex justify-center">
         <Button variant="contained" disabled>Continuar</Button>
