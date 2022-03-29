3ca378afee39da5f30977e099cc958604376adc4
ezequielleiter <ezequielleiter@gmail.com>
2022-03-29 11:20:22 -0300

    ajustes responsive de page archivo-notificaciones

--- src/components/common/ExpandableFilters.jsx
+++ src/components/common/ExpandableFilters.jsx
@@ -6,14 +6,21 @@ function ExpandableFilters({ buttonText = ['Ver mas filtros', 'Cerrar filtros'],
   const toggleExpand = () => setExpandedFilters(value => !value);
 
   return (
-    <Grid component={Card} container className="p-2 mb-4 mt-2 align-center">
-      {TopPart && <TopPart />}
-      <Grid item xs={3} className="d-flex justify-center">
-        <Button variant="text" onClick={toggleExpand}>{!expandedFilters ? buttonText[0] : buttonText[1]}</Button>
-      </Grid>
-      {expandedFilters && <ExpandablePart /> }
-      {BottomPart && <BottomPart />}
+   <Grid
+    component={Card}
+    container
+    className='p-2 mb-4 mt-2 align-center'
+    spacing={2}
+   >
+    {TopPart && <TopPart />}
+    <Grid item xs={3} className='d-flex justify-center'>
+     <Button variant='text' onClick={toggleExpand}>
+      {!expandedFilters ? buttonText[0] : buttonText[1]}
+     </Button>
     </Grid>
+    {expandedFilters && <ExpandablePart />}
+    {BottomPart && <BottomPart />}
+   </Grid>
   );
 }
 
