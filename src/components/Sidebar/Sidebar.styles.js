ae86f8a3c761b89f86c5833095bd0e4a98ee4f80
Julian Donsanto <julian.donsanto@stensul.com>
2022-04-04 18:24:05 -0300

    Menu toggled off in mobile with switch

---
+++ src/components/Sidebar/Sidebar.styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+import { drawerWidth } from '../../theme/greenTheme.js';
+import { Drawer } from '@mui/material';
+
+export const StyledDrawer = styled(Drawer)`
+  width: ${drawerWidth};
+  min-width: 240px;
+  flex-shrink: 0;
+  & .MuiDrawer-paper {
+    width: ${drawerWidth};
+    min-width: 240px;
+    box-sizing: border-box;
+    border: none;
+    box-shadow: 0px 4px 35px rgba(109, 102, 119, 0.2);
+  }
+`
