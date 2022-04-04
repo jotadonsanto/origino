66966170a4c2775e4f442b7cd489c9b3d61b69f9
Julian Donsanto <julian.donsanto@stensul.com>
2022-04-04 18:35:04 -0300

    Fixed container padding in mobile

--- src/layouts/Main.styles.js
+++ src/layouts/Main.styles.js
@@ -5,6 +5,9 @@ export const StyledContainer = styled(Container)`
   padding: 2rem 2rem 5rem 5rem;
   margin: 0;
   max-width: none;
+  @media (max-width: 600px) {
+    padding: 2rem!important;
+  }
 `
 export const StyledBox = styled(Box)`
   background: #FCFCFC;
